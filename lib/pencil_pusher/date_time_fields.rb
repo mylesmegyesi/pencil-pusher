1aa8e24d5fbeb63b41f8fac16fa755e2b3576387
skim <skim.la@gmail.com>
2014-08-07 09:41:22 +0100

    adds optional date time and optional time helpers

--- lib/pencil_pusher/date_time_fields.rb
+++ lib/pencil_pusher/date_time_fields.rb
@@ -1,4 +1,6 @@
 require 'pencil_pusher/validations/date_time_validator'
+require 'pencil_pusher/validations/optional_date_time_validator'
+require 'pencil_pusher/validations/optional_time_validator'
 require 'pencil_pusher/validations/time_validator'
 require 'pencil_pusher/virtus/coercer/date_time'
 require 'pencil_pusher/virtus/coercer/time'
@@ -21,6 +23,19 @@ module PencilPusher
         }
     end
 
+    def optional_date_time_field(options={})
+      field_name = options.fetch(:field_name)
+      format     = options.fetch(:format)
+
+      attribute field_name, PencilPusher::Virtus::Coercer::DateTime, format: format
+
+      validates field_name,
+        'pencil_pusher/validations/optional_date_time' => {
+          format:   format,
+          messages: {invalid: options.fetch(:invalid_message)}
+        }
+    end
+
     def required_time_field(options={})
       field_name = options.fetch(:field_name)
       format     = options.fetch(:format)
@@ -34,6 +49,19 @@ module PencilPusher
             blank:   options.fetch(:blank_message),
             invalid: options.fetch(:invalid_message)
           }
+        }
+    end
+
+    def optional_time_field(options={})
+      field_name = options.fetch(:field_name)
+      format     = options.fetch(:format)
+
+      attribute field_name, PencilPusher::Virtus::Coercer::Time, format: format
+
+      validates field_name,
+        'pencil_pusher/validations/optional_time' => {
+          format:   format,
+          messages: {invalid: options.fetch(:invalid_message)}
         }
     end
   end
